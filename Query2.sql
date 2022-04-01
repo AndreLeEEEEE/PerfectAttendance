1413b190b0a83ebef55c5ebebecf850dc8e603a6
AndreLeEEEEE <72272834+AndreLeEEEEE@users.noreply.github.com>
2022-04-01 08:23:31 -0600

    Downside information
    The amount of information presented in the results have been reduced to about half of its original size. It now displays the bare minimum: badge number, employee name, date, amount of hours worked in that shift, and amount of hours missing in that shift. All entries in the results should have a value larger than 0 for the missing hours column. Entries that would've had 0 missing hours aren't included in the results.

--- Query2.sql
+++ Query2.sql
@@ -1,5 +1,5 @@
 --"attendance_records" query -
---Find all late days and all missing hours
+--Find all days with missing full-time hours.
 --for every employee
 --from 1/1/2022 to today.
 --Work-in-Progress
@@ -8,80 +8,46 @@
 ---Actually combine split days, refer to query 'perfect_attendance' on how to achieve this
 
 SELECT
-TT.Badge_No,
-TT.Full_Name,
-TT.Employee_Status,
-TT.Description,
-TT.Date,
-TT.Scheduled_In_Time,
-TT.Scheduled_Out_Time,
---Remove overtime hours from scheduled clock out time
-DATEADD(MI, TT.Scheduled_OT, TT.Scheduled_Out_Time) as Adjusted_Scheduled_Out_Time,
-TT.Real_In_Time,
-TT.Real_Out_Time,
-SUM(CASE
-  WHEN DATEDIFF(MI, TT.Scheduled_In_Time, DATEADD(MI, TT.Scheduled_OT, TT.Scheduled_Out_Time)) > 480.00 THEN
-    ROUND((DATEDIFF(MI, TT.Scheduled_In_Time, DATEADD(MI, TT.Scheduled_OT, TT.Scheduled_Out_Time)) - 30.0) / 60.0, 2) - TT.Regular_Hours
+Hours.Badge_No,
+Hours.Name,
+Hours.Date,
+Hours.Worked_Hours,
+CASE
+  WHEN 8.0 - Hours.Worked_Hours > 0 THEN
+    8.0 - Hours.Worked_Hours
   ELSE
-    ROUND(DATEDIFF(MI, TT.Scheduled_In_Time, DATEADD(MI, TT.Scheduled_OT, TT.Scheduled_Out_Time)) / 60.0, 2) - TT.Regular_Hours
-END) as Missing_Hours,
-SUM(TT.Regular_Hours) as Actual_Hours,
-TT.Note
+    0.0
+END AS Missing_Hours
 
 FROM (
-	SELECT
-    EMP.Badge_No,
-    PU.First_Name + ' ' + PU.Last_Name AS 'Full_Name',
-    EMP.Employee_Status,
-    CT.Description,
-    CAST(Clockin.Pay_Date as DATE) as Date,
-    CT.Absent,
-    --Amount of hours to subtract from Scheduled_Out_Time to yield exactly 8 scheduled hours and 30 minutes for lunch if applicable
-    --Negative number
-    CASE
-      --If larger than 8.5 hours
-      WHEN ABS(DATEDIFF(MI, Clockin.Scheduled_In_Time, Clockin.Scheduled_Out_Time)) > 510.00 THEN
-        DATEDIFF(MI, Clockin.Scheduled_Out_Time, Clockin.Scheduled_In_Time) + 510.00
-      ELSE
-        0.00
-    END as Scheduled_OT,
-    
-    --Convert from Eastern time to Mountain time
-    DATEADD(HOUR, -2, Clockin.Scheduled_In_Time) as Scheduled_In_Time,
-    DATEADD(HOUR, -2, Clockin.Scheduled_Out_Time) as Scheduled_Out_Time,
-    DATEADD(HOUR, -2, Clockin.Clockin_Time) as Real_In_Time,
-    DATEADD(HOUR, -2, Clockin.Clockout_Time) as Real_Out_Time,
-    Clockin.Note,
-    Clockin.Regular_Hours,
-    Clockin.Shift_Hours
+  SELECT
+  EMP.Badge_No,
+  PU.First_Name + ' ' + PU.Last_Name AS Name,
+  CAST(Clockin.Pay_Date AS DATE) AS Date,
+  SUM(Clockin.Regular_Hours) as Worked_Hours
   
-	FROM Personnel_v_Clockin Clockin
-	JOIN Personnel_v_Employee EMP
-	  ON Clockin.Plexus_User_No = EMP.Plexus_User_No
-	JOIN Personnel_v_Clockin_Type CT
-	  ON Clockin.Clockin_Type_Key = CT.Clockin_Type_Key
-	JOIN Plexus_Control_v_Plexus_User PU
-	  ON EMP.Plexus_User_No = PU.Plexus_User_No
-	  
-	WHERE Clockin.Scheduled_In_Time BETWEEN '2022-01-01' AND GETDATE()
-	AND EMP.Badge_No > 0
-	AND (CT.Clockin_Type_Key = 393 --Suspended
-	OR CT.Clockin_Type_Key = 9722 --Unpaid
-	OR CT.Clockin_Type_Key = 386 --Work
-	OR CT.Clockin_Type_Key = 387) --PTO
-	AND Clockin.Regular_Hours <
-	CASE WHEN Clockin.Shift_Hours > 8.00 THEN
-	  8.00
-	ELSE
-	  Clockin.Shift_Hours
-	END
-	AND DATENAME(dw, Clockin.Scheduled_In_Time) <> 'Saturday'
-  AND DATENAME(dw, Clockin.Scheduled_In_Time) <> 'Sunday'
-) AS TT
+  FROM Personnel_v_Clockin Clockin
+    JOIN Personnel_v_Employee EMP
+      ON Clockin.Plexus_User_No = EMP.Plexus_User_No
+    JOIN Personnel_v_Clockin_Type CT
+      ON Clockin.Clockin_Type_Key = CT.Clockin_Type_Key
+    JOIN Plexus_Control_v_Plexus_User PU
+      ON EMP.Plexus_User_No = PU.Plexus_User_No
+    
+  WHERE Clockin.Pay_Date BETWEEN '2022-01-01' AND GETDATE()
+  AND EMP.Badge_No > 0
+  AND DATENAME(dw, Clockin.Pay_Date) <> 'Saturday'
+  AND DATENAME(dw, Clockin.Pay_Date) <> 'Sunday'
+  AND CT.Clockin_Type_Key <> 395
+  AND CT.Clockin_Type_Key <> 9723
+  AND CT.Clockin_Type_Key <> 10804
+  AND CT.Clockin_Type_Key <> 10805
+  AND CT.Clockin_Type_Key <> 10806
+  AND CT.Clockin_Type_Key <> 10807
+  AND PU.Active <> 0
+  
+  GROUP BY EMP.Badge_No, PU.First_Name, PU.Last_Name, Clockin.Pay_Date
+  --ORDER BY EMP.Badge_No, Clockin.Pay_Date
+) AS Hours
 
-GROUP BY TT.Badge_No, TT.Full_Name, TT.Employee_Status,
-TT.Description, TT.Date, TT.Scheduled_In_Time,
-TT.Scheduled_Out_Time, TT.Scheduled_OT, TT.Real_In_Time,
-TT.Real_Out_Time,  TT.Note
-
-ORDER BY TT.Badge_No, CAST(DATEADD(HOUR, -2, TT.Scheduled_In_Time) as DATE);
+WHERE Hours.Worked_Hours < 8.0;
